ea79e6b758e2bc14ef37f62df128addca93dc300
Rodrigo Speller <rodrigo@primosti.com.br>
2023-03-27 17:22:39 -0300

    Proof of Concept

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Rodrigo Speller. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+import { getInput } from '@actions/core'
+
+console.log('Hello World')
+console.log('Image:', getInput('image'));
